1b51d439be76c6a937c92156e9e2378a69d1f49d
trieuhoangtuan <trieuhoangtuan1996@gmail.com>
2017-05-19 19:20:14 +0700

    chỉnh sửa lần 1

---
+++ bai 52/bai 52.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+float Nhap(float a, float b, float c, float d, float e, float f);
+
+float Nhap(float a, float b, float c, float d, float e, float f)
+{
+	float a,b,c,d,e,f, x, x1, y, y1, x2, y2;
+	a*x1+b*y1=c;
+}
